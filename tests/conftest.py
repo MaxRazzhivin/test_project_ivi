d5402b75d02977851018bd00aac68c3c1976766b
Max_Razzhivin <max.nvo06@gmail.com>
2025-03-23 19:26:23 +0300

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,44 +16,45 @@ def load_env():
     load_dotenv()
 
 
-# def pytest_addoption(parser):
-#     parser.addoption(
-#         '--browser_version',
-#         default='128.0'
-#     )
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='128.0'
+    )
 
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
 
     # Настройка опций для режима инкогнито
-    options = webdriver.ChromeOptions()
-    options.add_argument('--incognito')
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--incognito')
 
-    # Настройка, чтобы разрешение экрана было максимальным для текущего устройства
-    browser.driver.maximize_window()
+    # Устанавливаем разрешение экрана
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
-    # browser_version = request.config.getoption('--browser_version')
-    # options = Options()
-    # selenoid_capabilities = {
-    #     "browserName": "chrome",
-    #     "browserVersion": browser_version,
-    #     "selenoid:options": {
-    #         "enableVNC": True,
-    #         "enableVideo": True
-    #     }
-    # }
+    browser_version = request.config.getoption('--browser_version')
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
 
     selenoid_login = os.getenv("SELENOID_LOGIN")
     selenoid_pass = os.getenv("SELENOID_PASS")
     selenoid_url = os.getenv("SELENOID_URL")
 
-    # options.capabilities.update(selenoid_capabilities)
-    # driver = webdriver.Remote(
-    #     command_executor=f'https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub',
-    #     options=options)
-    #
-    # browser.config.driver = driver
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f'https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub',
+        options=options)
+
+    browser.config.driver = driver
 
     yield
 
